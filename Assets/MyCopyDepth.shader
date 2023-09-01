5754e44b757dd4dd680530c450cf3705cc464f65
dosusang <2501488984@qq.com>
2023-09-01 16:42:40 +0800

    first commit

---
+++ Assets/MyCopyDepth.shader
@@ -0,0 +1,51 @@
+﻿Shader "Unlit/MyCopyDepth"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            ZTest Always ZWrite On ColorMask 0
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            UNITY_DECLARE_DEPTH_TEXTURE(_MyDepthTex) ;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            float frag (v2f i) : SV_Depth
+            {
+                float col = SAMPLE_DEPTH_TEXTURE(_MyDepthTex, i.uv);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Test : MonoBehaviour
+{
+    public Camera cam;
+    public RenderTexture depth;
+    public Material mat;
+    public Material CopyDepthMat;
+
+    public RenderTexture SrcColorTex;
+    public RenderTexture SrcDepthTex;
+
+    private void OnPreRender()
+    {
+        SrcDepthTex.DiscardContents();
+        cam.SetTargetBuffers(SrcColorTex.colorBuffer, SrcDepthTex.depthBuffer);
+    }
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        SrcColorTex = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.RGB111110Float);
+        SrcColorTex.name = "SrcColorTex";
+        SrcColorTex.Create();
+        
+        SrcDepthTex = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
+        SrcDepthTex.name = "SrcDepthTex";
+        SrcDepthTex.Create();
+        
+        depth = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
+        var cmd = new CommandBuffer();
+
+        mat.mainTexture = depth;
+        cam = GetComponent<Camera>();
+        
+        CopyDepthMat.SetTexture("_MyDepthTex", SrcDepthTex);
+        cmd.Blit(SrcDepthTex, depth, CopyDepthMat);
+        cmd.Blit(SrcColorTex, BuiltinRenderTextureType.CameraTarget);
+
+        cam.AddCommandBuffer(CameraEvent.AfterForwardOpaque,cmd);
+    }
+}
